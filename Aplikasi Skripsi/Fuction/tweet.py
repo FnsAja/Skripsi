36a9f52ff118e8dd91e57a6f4ce14572f06162bb
ImFns <michaelalfonso95@gmail.com>
2023-03-04 17:35:27 +0700

    Update fungsi

--- Aplikasi Skripsi/Fuction/tweet.py
+++ Aplikasi Skripsi/Fuction/tweet.py
@@ -16,7 +16,7 @@ def appendData(query):
     for tweet in sntwitter.TwitterSearchScraper(query, maxEmptyPages=1000).get_items():
         print(tweet.date)
         tweets.append([str(tweet.date).split(' ')[0], ' ', re.sub('http://\S+|https://\S+|@\S+|#\S+|##\S+', '', str(tweet.rawContent))])
-    
+
     return tweets
 
 def insertToExcel(tweets, name):
@@ -25,22 +25,37 @@ def insertToExcel(tweets, name):
     df3 = pd.DataFrame(tweets[2], columns=['Date', 'Sentiment', 'Tweet']).drop_duplicates(subset='Tweet')
     if os.path.exists(f"output_{name}.xlsx"):
         os.remove(f"output_{name}.xlsx")
-    
-    with pd.ExcelWriter(f"output_{name}.xlsx") as excel:
-        df1.to_excel(excel, sheet_name='January')
-        df2.to_excel(excel, sheet_name='February')
-        df3.to_excel(excel, sheet_name='March')
+        
+    df1.to_excel(f"output_{name}.xlsx_Jan")
+    df2.to_excel(f"output_{name}.xlsx_Feb")
+    df3.to_excel(f"output_{name}.xlsx_Mar")
         
     print("success")
 
-name = "Prabowo Subianto"
-tweets = query(name)
-insertToExcel(tweets, name)
+def insertToCSV(tweets, name):
+    df1 = pd.DataFrame(tweets[0], columns=['Date', 'Sentiment', 'Tweet']).drop_duplicates(subset='Tweet')
+    df2 = pd.DataFrame(tweets[1], columns=['Date', 'Sentiment', 'Tweet']).drop_duplicates(subset='Tweet')
+    df3 = pd.DataFrame(tweets[2], columns=['Date', 'Sentiment', 'Tweet']).drop_duplicates(subset='Tweet')
+    if os.path.exists(f"output_{name}.csv"):
+        os.remove(f"output_{name}.csv")
+        
+    df1.to_csv(f"output_{name}.csv_Jan")
+    df2.to_csv(f"output_{name}.csv_Feb")
+    df3.to_csv(f"output_{name}.csv_Mar")
+        
+    print("success")
 
-name = "Anies Baswedan"
-tweets = query(name)
-insertToExcel(tweets, name)
+# name = "Prabowo Subianto"
+# tweets = query(name)
+# insertToExcel(tweets, name)
+# insertToCSV(tweets, name)
 
-name = "Prabowo Subianto"
-tweets = query(name)
-insertToExcel(tweets, name)
+# name = "Anies Baswedan"
+# tweets = query(name)
+# insertToExcel(tweets, name)
+# insertToCSV(tweets, name)
+
+# name = "Prabowo Subianto"
+# tweets = query(name)
+# insertToExcel(tweets, name)
+# insertToCSV(tweets, name)
