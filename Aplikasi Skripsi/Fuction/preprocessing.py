4bd992995398420288af8ccaa5977a27bf4fcb7e
FnsAja <michaelalfonso95@gmail.com>
2023-03-07 23:22:07 +0700

    Update Sinonim (Gagal)

--- Aplikasi Skripsi/Fuction/preprocessing.py
+++ Aplikasi Skripsi/Fuction/preprocessing.py
@@ -1,10 +1,11 @@
 import pandas as pd 
 import re
+import "https://github.com/victoriasovereigne/tesaurus.git"
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn import svm, metrics
 
 data_source_url = "./Datatest.csv"
@@ -42,26 +43,32 @@ for sentences in features:
     
     # Remove Stop Words
     stopwords_result = [word for word in token_result if not word in list_stopwords]
-
+    print("Before : ", stopwords_result)
+    
+    # Synonym Words
+    synonym = map(tesaurus.getSinonim(), stopwords_result)
+    print("Sinonim : ", synonym)
+    
     # Back To String
     temporary = ' '.join(map(str, stopwords_result))
 
     # Tambahkan kedalam list
     processed_features.append(temporary)
 
-vectorizer = TfidfVectorizer()
-X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.4, random_state=0)
-train_processed_features = vectorizer.fit_transform(X_train)
-test_processed_features = vectorizer.transform(X_test)
+# vectorizer = TfidfVectorizer()
+# for i in range(1, 9):
+#     X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=i * 0.1, random_state=0)
+#     train_processed_features = vectorizer.fit_transform(X_train)
+#     test_processed_features = vectorizer.transform(X_test)
 
-clf = svm.SVC(kernel="rbf")
+#     clf = svm.SVC(kernel="rbf")
 
-# Train Model using fit()
-clf.fit(train_processed_features, y_train)
+#     # Train Model using fit()
+#     clf.fit(train_processed_features, y_train)
 
-# Predict
-y_pred = clf.predict(test_processed_features)
+#     # Predict
+#     y_pred = clf.predict(test_processed_features)
 
-print("Accuracy : ", metrics.accuracy_score(y_test, y_pred))
-print("Precision : ", metrics.precision_score(y_test, y_pred, average='micro'))
-print("Recall : ", metrics.recall_score(y_test, y_pred, average='micro'))
+#     print("Accuracy K-" + i + " : ", metrics.accuracy_score(y_test, y_pred))
+#     print("Precision : ", metrics.precision_score(y_test, y_pred, average='micro'))
+#     print("Recall : ", metrics.recall_score(y_test, y_pred, average='micro'))
