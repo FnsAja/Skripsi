a783d68c83db883abfe7e7c15402dcd9ef8274f9
ImFns <michaelalfonso95@gmail.com>
2023-04-10 18:18:26 +0700

    Implementasi Chart

--- Aplikasi Skripsi/Function/preprocessing.py
+++ Aplikasi Skripsi/Function/preprocessing.py
@@ -209,18 +209,15 @@ def trainModel(labels, processed_features):
         rowLen = numpy.unique(rows)
         for i in range(0, len(rowLen)):
             df1 = pd.DataFrame({'X': [df.loc[df['row'] == i, 'data'].head(2).iloc[0]], 'Y': [df.loc[df['row'] == i, 'data'].head(2).iloc[1]]})
-            result = result.append(df1.values, ignore_index=True, index=i)
-
-        print(result)
-          
-        data2D = X_test[:2, :]  
-        xx, yy = numpy.meshgrid(numpy.linspace(data2D[:, 0].min(), data2D[:, 0].max()), numpy.linspace(data2D[:, 1].min(), data2D[:, 1].max()))  
+            result = result.append(list(df1.values), ignore_index=True)
+  
+        xx, yy = numpy.meshgrid(numpy.linspace(result[:, 0].min(), result[:, 0].max()), numpy.linspace(result[:, 1].min(), result[:, 1].max()))  
         grid = numpy.vstack([xx.ravel(), yy.ravel()]).T  
-        model = clf.fit(data2D[:, :2], y_test)  
+        model = clf.fit(result[:, :2], y_test)  
         y_pred = numpy.reshape(model.predict(grid), xx.shape)          
         display = DecisionBoundaryDisplay(xx0=xx, xx1=yy, response=y_pred)  
         display.plot()  
-        display.ax_.scatter(data2D[:, 0], data2D[:, 1], c=y_test, edgecolors="black")  
+        display.ax_.scatter(result[:, 0], result[:, 1], c=y_test, edgecolors="black")  
         plt.savefig('TrainChart.png')  
   
         countNetral = 0  
